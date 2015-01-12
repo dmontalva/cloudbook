e05e6a0db06c2441ac59c0716b0ef3783b6c4ca0
Raul Rodrigo <raurodse@gmail.com>
2015-01-12 18:36:24 +0100

    wip

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,27 @@
-function load_element(element){
-            var content=document.getElementById("targetpage");
-            content.innerHTML = elements[element.id];
+function load_element(){
             $(function() {
-$( ".draggable" ).draggable();
-});
-}
-$("#save").bind('click',function(){
-	console.log($("#targetpage").html());
-	var fs = require("fs");
-	fs.writeFile('/home/kbut/inboxubuntu/cloudbook',$("#targetpage").html());
-});
-var elements = {
-    "a":'<h1 class="draggable">cabecera</h1><textarea>Esto es el primer parrafo</textarea>',
-    "b":"<h2>cabecera</h2><p>Esto es el primer parrafo</p><p>Esto es el segundo parrafo</p>",
-    "c":"<h1>cabecera</h1><p>Esto es el primer parrafo</p>",
-    "d":"<h2>cabecera</h2><p>Esto es el primer parrafo</p><p>Esto es el segundo parrafo</p>",
-    "e":"<h1>cabecera</h1><p>Esto es el primer parrafo</p>",
-    "f":"<h2>cabecera</h2><p>Esto es el primer parrafo</p><p>Esto es el segundo parrafo</p>",
+                $( ".draggable" ).draggable();
+            });
 }
 
+function append_textbox(){
+    var aux = $(document.createElement('div')).html('Esto es una prueba de concepto');
+    aux.css('position','relative');
+    aux.css('top',100);
+    aux.css('left',100);
+    aux.addClass('draggable');
+    aux.dblclick(function(event) {
+        $(this).css('border','2px solid black');
+    });
+    $("#targetcontent").append(aux);
+    load_element();
+}
+
+
+function binding_actions(){
+    $("#actaddtext").bind('click',append_textbox);
+}
+
+$(document).ready(function(){
+    binding_actions();
+});
