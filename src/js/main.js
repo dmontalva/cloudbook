3f7548dec319921995fa902eb2df0f32ede2cbc6
Raul Rodrigo <raurodse@gmail.com>
2015-01-15 13:41:50 +0100

    Added support to components

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,12 @@
+var fs = require("fs");
+
 function load_element(){
             $(function() {
                 $( ".draggable" ).draggable();
             });
 }
 
-function append_textbox(){
-    var aux = $(document.createElement('div')).html('Esto es una prueba de concepto');
-    aux.css('position','relative');
-    aux.css('top',100);
-    aux.css('left',100);
-    aux.addClass('draggable');
-    aux.dblclick(function(event) {
-        $(this).css('border','2px solid black');
-    });
-    $("#targetcontent").append(aux);
-    load_element();
+function appendsection(element){
+    var newsection = $(document.createElement('img')).attr('src','img/1.png');
+    $(element).before(newsection);
 }
-
-
-function binding_actions(){
-    $("#actaddtext").bind('click',append_textbox);
-}
-
-$(document).ready(function(){
-    binding_actions();
-});
